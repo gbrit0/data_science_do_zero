eba55c8deaa20830bb5cdac99d11fbdbc164268e
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2024-01-31 10:01:43 -0300

    Capítulo 11 - Aprendizado de Máquina

--- cap11.py
+++ cap11.py
@@ -46,3 +46,29 @@ assert len(x_test) == len(y_test) == 250
 # Verifique se os pontos de dados correspondentes estão emparelhados corretamente
 assert all(y == 2 * x for x, y in zip(x_train, y_train))
 assert all(y == 2 * x for x, y in zip(x_test, y_test))
+
+
+# Correção
+
+def accuracy(tp: int, fp: int, fn: int, tn: int) -> float:
+   correct = tp + tn
+   total = tp + fp + fn + tn
+   return correct / total
+
+assert accuracy(70, 4930, 13930, 981070) == 0.98114
+
+def precision(tp: int, fp: int, fn: int, tn: int) -> float:
+   return tp / (tp+fp)
+
+assert precision(70, 4930, 13930, 981070) == 0.014
+
+def recall(tp: int, fp: int, fn: int, tn: int) -> float:
+   return tp / (tp + fn)
+
+assert recall(70, 4930, 13930, 981070) == 0.005, f"{recall(70, 4930, 13930, 981070)}"
+
+def f1_score(tp: int, fp: int, fn: int, tn: int) -> float:
+   p = precision(tp, fp, fn, tn)
+   r = recall(tp, fp, fn, tn)
+
+   return 2 * p * r / (p + r)
