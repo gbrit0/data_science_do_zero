112e184483aae7446a530be83535b88b3fadea1b
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2024-02-06 10:08:03 -0300

    Capítulo 15 - Regressão Múltipla

--- cap15.py
+++ cap15.py
@@ -83,7 +83,7 @@ Stat = TypeVar('Stat')  # tipo genérico para "estatísticas"
 
 def bootstrap_sample(data: List[X]) -> List[X]:
    """Tira amostras aleatoriamente de elementos len(data) com substituição"""
-   return[random.shoice(data) for _ in data]
+   return[random.choice(data) for _ in data]
 
 def bootstrap_statistics(data: List[X],
                          stats_fn: Callable[[List[X]], Stat],
@@ -91,3 +91,136 @@ def bootstrap_statistics(data: List[X],
    """avlia stats_fn em amostras de bootstrap num_samples dos dados"""
    return [stats_fn(bootstrap_sample(data)) for _ in range(num_samples)]
 
+# 101 pontos muito próximos de 100
+close_to_100 = [99.5 + random.random() for _ in range(101)]
+
+#101 pontos, 50 próximos de 0, 50 próximos de 200
+far_from_100 = ([99.5 + random.random()] + 
+                [random.random() for _ in range(50)] + 
+                [200 + random.random() for _ in range(50)])
+
+from cap05 import median, standard_deviation
+
+medians_close = bootstrap_statistics(close_to_100, median, 100)
+
+medians_far = bootstrap_statistics(far_from_100, median, 100)
+
+
+assert standard_deviation(medians_close) < 1
+assert standard_deviation(medians_far) > 90
+
+from typing import Tuple
+
+import datetime
+
+def estimate_sample_beta(pairs: List[Tuple[Vector, float]]):
+   x_sample = [x for x, _ in pairs]
+   y_sample = [y for _, y in pairs]
+   beta = least_squares_fit(x_sample, y_sample, learning_rate, 5000, 25)
+   print("bootsrap sample", beta)
+   return beta
+
+# Isso demora alguns minutos
+bootstrap_betas = bootstrap_statistics(list(zip(inputs, daily_minutes_good)),
+                                        estimate_sample_beta,
+                                        100)
+
+bootstrap_standard_errors = [
+   standard_deviation([beta[i] for beta in bootstrap_betas])
+   for i in range(4)]
+
+print(bootstrap_standard_errors)
+
+from cap06 import normal_cdf
+
+def p_value(beta_hat_j: float, sigma_hat_j: float) -> float:
+   if beta_hat_j > 0:
+      # se o coeficiente for positivo, temos que computar o dobro
+      # da probabilidade de ver um valor *maior* do que esse
+      return 2 * (1 - normal_cdf(beta_hat_j / sigma_hat_j))
+   else:
+      # caso contrário, o dobro da probabilidade de ver um valor *menor*
+      return 2* normal_cdf(beta_hat_j / sigma_hat_j)
+   
+assert p_value(30.58, 1.27)   < 0.001  # constant term
+assert p_value(0.972, 0.103)  < 0.001  # num_friends
+assert p_value(-1.865, 0.155) < 0.001  # work_hours
+assert p_value(0.923, 1.249)  > 0.4    # phd
+
+# REGULARIZAÇÃO
+
+# alpha é um hiperparâmetro que controla o rigor da penalidade.
+# Às vezes, ele é chamado de lambda, mas esse símbolo já tem significado no Python
+def ridge_penalty(beta: Vector, alpha: float) -> float:
+   return alpha * dot(beta[1:], beta[1:])
+
+def squared_error_ridge(x: Vector,
+                        y: float,
+                        beta: Vector,
+                        alpha: float) -> float:
+   """Estime o erro mais a penalidade de cume em beta"""
+   return error(x, y, beta) ** 2 + ridge_penalty(beta, alpha)
+
+# conectando isso ao gradiente descendente
+from cap04 import add
+
+def ridge_penalty_gradient(beta: Vector, alpha: float) -> Vector:
+   """Gradiente apenas da penalidade de cume"""
+   return [0.] + [2 * alpha * beta_j for beta_j in beta[1:]]
+
+def sqerror_ridge_gradient(x: Vector,
+                           y: float,
+                           beta: Vector,
+                           alpha: float) -> Vector:
+   """
+   O gradiente correspondente ao termo de erro quadrático i,
+   incluindo a penalidade de cume y
+   """
+   return add(sqerror_gradient(x, y, beta),
+              ridge_penalty_gradient(beta, alpha))
+
+def least_squares_fit_ridge(xs: List[Vector],
+                            ys: List[float],
+                            alpha: float,
+                            learning_rate: float,
+                            num_steps: int,
+                            batch_size: int = 1) -> Vector:
+    # Start guess with mean
+    guess = [random.random() for _ in xs[0]]
+
+    for i in range(num_steps):
+        for start in range(0, len(xs), batch_size):
+            batch_xs = xs[start:start+batch_size]
+            batch_ys = ys[start:start+batch_size]
+
+            gradient = vector_mean([sqerror_ridge_gradient(x, y, guess, alpha)
+                                    for x, y in zip(batch_xs, batch_ys)])
+            guess = gradient_step(guess, gradient, -learning_rate)
+
+    return guess
+
+beta_0 = least_squares_fit_ridge(inputs, daily_minutes_good, 0.0, #alpha
+                                 learning_rate, 5000, 25)
+# [30.514484888280567, 0.9748440502839583, -1.8506798946882428, 0.9143200926682622]
+
+assert 5 < dot(beta_0[1:], beta_0[1:]) < 6
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_0) < 0.69
+
+beta_0_1 = least_squares_fit_ridge(inputs, daily_minutes_good, 0.1,  # alpha
+                                 learning_rate, 5000, 25)
+# [30.8, 0.95, -1.83, 0.54]
+assert 4 < dot(beta_0_1[1:], beta_0_1[1:]) < 5
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_0_1) < 0.69
+
+
+beta_1 = least_squares_fit_ridge(inputs, daily_minutes_good, 1,  # alpha
+                                 learning_rate, 5000, 25)
+# [30.6, 0.90, -1.68, 0.10]
+assert 3 < dot(beta_1[1:], beta_1[1:]) < 4
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_1) < 0.69
+
+beta_10 = least_squares_fit_ridge(inputs, daily_minutes_good,10,  # alpha
+                                 learning_rate, 5000, 25)
+# [28.3, 0.67, -0.90, -0.01]
+assert 1 < dot(beta_10[1:], beta_10[1:]) < 2
+assert 0.5 < multiple_r_squared(inputs, daily_minutes_good, beta_10) < 0.6
