39e0dc13fec556c70b4c1c82104362105a14694a
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2024-02-23 11:40:38 -0300

    Capítulo 21 - NLP

--- cap21.py
+++ cap21.py
@@ -128,7 +128,8 @@ def expand(grammar: Grammar, tokens: List[str]) -> List[str]:
 def generate_sentence(grammar: Grammar) -> List[str]:
    return expand(grammar, ["_S"])
 
-from typing import Tuple, random
+from typing import Tuple
+import random
 
 def roll_a_die() -> int:
    return random.choice([1, 2, 3, 4, 5, 6])
@@ -166,3 +167,277 @@ def compare_distibutions(num_samples: int = 1000) -> Dict[int, List[int]]:
       counts[direct_sample()][1] += 1
    return counts
 
+def sample_from(weights: List[float]) -> int:
+   """Retorna i com probabilidade weights[i] / sum(weigthts)"""
+   total = sum(weights)
+   rnd = total * random.random()      # uniforme entre 0 e total
+   for i, w in enumerate(weights):
+      rnd -= w
+      if rnd <= 0: return i
+
+from collections import Counter
+# Execute mil vezes e conte
+draws = Counter(sample_from([0.1, 0.1, 0.8]) for _ in range(1000))
+
+assert 10 < draws[0] < 190 # deve ser ~10%, o teste é muito flexível
+assert 10 < draws[1] < 190 # deve ser ~10%, o teste é muito flexível
+assert 650 < draws[2] < 950 # deve ser ~80%, o teste é muito flexível
+assert draws[0] + draws[1] + draws[2] == 1000
+
+documents = [
+   ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
+   ["NoSQL", "MongoDB", "Cassandra", "HBase", "Postgres"],
+   ["Python", "scikit-learn", "scipy", "numpy", "statsmodels", "pandas"],
+   ["R", "Python", "statistics", "regression", "probability"],
+   ["machine learning", "regression", "decision trees", "libsvm"],
+   ["Python", "R", "Java", "C++", "Haskell", "programming languages"],
+   ["statistics", "probability", "mathematics", "theory"],
+   ["machine learning", "scikit-learn", "Mahout", "neural networks"],
+   ["neural networks", "deep learning", "Big Data", "artificial intelligence"],
+   ["Hadoop", "Java", "MapReduce", "Big Data"],
+   ["statistics", "R", "statsmodels"],
+   ["C++", "deep learning", "artificial intelligence", "probability"],
+   ["pandas", "R", "Python"],
+   ["databases", "HBase", "Postgres", "MySQL", "MongoDB"],
+   ["libsvm", "regression", "support vector machines"]
+]
+
+K = 4
+
+# Quantas vezes cada tópico é atribuído a cada documento
+# uma lista de Counters, um para cada documento
+document_topic_counts = [Counter() for _ in documents]
+
+# Quantas vezes cada palavra é atribuída a cada tópico
+# uma lisra de Counters, um para cada tópico
+topic_word_counts = [Counter() for _ in range(K)]
+
+# O número total de palavras atribuídas a cada tópico
+# uma lista de números, um para cada tópico
+topic_counts = [0 for _ in range(K)]
+
+# O número total de palavras contidas em cada documento
+# uma lista de números, um para cada documento
+document_lenghts =[len(document) for document in documents]
+
+# O número de palavras distintas
+distinct_words = set(word for document in documents for word in document)
+W = len(distinct_words)
+
+# o número de documentos
+D = len(documents)
+
+
+def p_topic_given_document(topic: int, d: int, alpha: float = 0.1) -> float:
+   """
+   A fração de palavras no documento 'd' atribuídas ao 'tópico' (mais a suavização)
+   """
+   return ((document_topic_counts[d][topic] + alpha) /
+           (document_lenghts[d] + K * alpha))
+
+
+def p_word_given_topic(word: str, topic: int, beta: float = 0.1) -> float:
+   """
+   A fração de palavras atribuídas ao 'tópico' iguais à 'palavra' (mais a suavização)
+   """
+   return ((topic_word_counts[topic][word] + beta) /
+           (topic_counts[topic] + W * beta))
+
+
+def topic_weight(d: int, word: str, k: int) -> float:
+   """
+   Para um certo documento e uma certa palavra nesse documento, retorne o peso do tópico k
+   """
+   return p_word_given_topic(word, k) * p_topic_given_document(k, d)
+
+
+def choose_new_topic(d: int, word: str) -> str:
+   return sample_from([topic_weight(d, word, k)
+                       for k in range(K)])
+
+
+random.seed(0)
+document_topics = [[random.randrange(K) for word in document]
+                   for document in documents]
+
+for d in range(D):
+   for word, topic in zip(documents[d], document_topics[d]):
+      document_topic_counts[d][topic] += 1
+      topic_word_counts[topic][word] += 1
+      topic_counts[topic] += 1
+
+import tqdm
+
+for iter in tqdm.trange(1000):
+   for d in range(D):
+      for i, (word, topic) in enumerate(zip(documents[d], document_topics[d])):
+
+         # remova esta palavra/tópico das contagens para que naõ influencie os pesos
+         document_topic_counts[d][topic] -= 1
+         topic_word_counts[topic][word] -= 1
+         topic_counts[topic] -= 1
+         document_lenghts[d] -= 1
+
+         # escolha um novo tópico com base nos pesos
+         new_topic = choose_new_topic(d, word)
+         document_topics[d][i] = new_topic
+
+         # e agora o adicione novamente às contagens
+         document_topic_counts[d][new_topic] += 1
+         topic_word_counts[new_topic][word] += 1
+         topic_counts[new_topic] += 1
+         document_lenghts[d] += 1
+
+
+for k, word_counts in enumerate(topic_word_counts):
+   for word, count in word_counts.most_common():
+      if count > 0:
+         print(k, word, count)
+
+
+from cap04 import dot, Vector
+import math
+
+def cosine_similarity(v1: Vector, v2: Vector) -> float:
+   return dot(v1, v2) / math.sqrt(dot(v1, v1) * dot(v2, v2))
+
+assert cosine_similarity([1., 1, 1], [2., 2, 2]) == 1, "mesma direção"
+assert cosine_similarity([-1, -1], [2., 2]) == -1, "direção oposta"
+assert cosine_similarity([1., 0], [0., 1]) == 0, "ortogonal"
+
+colors = ["red", "green", "blue", "yellow", "black", ""]
+nouns = ["bed", "car", "boat", "cat"]
+verbs = ["is", "was", "seems"]
+adverbs = ["very", "quite", "extremely", ""]
+adjectives = ["slow", "fast", "soft", "hard"]
+
+def make_sentence() -> str:
+   return " ".join([
+      "The",
+      random.choice(colors),
+      random.choice(nouns),
+      random.choice(verbs),
+      random.choice(adverbs),
+      random.choice(adjectives),
+      "."
+   ])
+
+NUM_SENTENCES = 50
+
+random.seed(0)
+sentences = [make_sentence() for _ in range(NUM_SENTENCES)]
+
+from cap19 import Tensor
+
+class Vocabulary:
+   def __init__(self, words: List[str] = None) -> None:
+      self.w2i: Dict[str, int] = {}          # mapeamento palavra -> word_id
+      self.i2w: Dict[int, str] = {}          # mapeamento word_id -> palavra
+
+      for word in (words or []):             # Se houver palavras, adicione-as
+         self.add(word)
+
+   @property
+   def size(self) -> int:
+      """Há quantas palavras no vocabulário"""
+      return len(self.w2i)
+   
+   def add(self, word: str) -> None:
+      if word not in self.w2i:                  # Se a palavra for nova:
+         word_id = len(self.w2i)                # Encontre o próximo ID.
+         self.w2i[word] = word_id               # Adicione ao mapa palavra -> word_id.
+         self.i2w[word_id] = word               # Adicione ao mapa word_id -> palavra.
+
+   def get_id(self, word: str):
+      """Retorne o id da palavra (ou None)"""
+      return self.w2i.get(word)
+   
+   def get_word(self, word_id: int) -> str:
+      """Retorne a palavra com o id fornecido (ou None)"""
+      return self.i2w.get(word_id)
+   
+   def one_hot_encode(self, word:str) -> Tensor:
+      word_id = self.get_id(word)
+      assert word_id is not None, f"unknown word {word}"
+
+      return [1.0 if i == word_id else 0.0 for i in range(self.size)]
+   
+vocab = Vocabulary(["a", "b", "c"])
+assert vocab.size == 3, "há 3 palavras no vocabulário"
+assert vocab.get_id("b") == 1, "b deve ter word_id 1"
+assert vocab.one_hot_encode("b") == [0, 1, 0]
+assert vocab.get_id("z") is None, "z não está no vocabulário"
+assert vocab.get_word(2) == "c", "word_id 2 deve ser c"
+vocab.add("z")
+assert vocab.size == 4, "agora  há 4 palavras no vocabulário"
+assert vocab.get_id("z") == 3, "agora z deve trer o id 3"
+assert vocab.one_hot_encode("z") == [0, 0, 0, 1]
+
+import json
+
+def save_vocab(vocab: Vocabulary, filename: str) -> None:
+   with open(filename, 'w') as f:
+      json.dump(vocab.w2i, f)       # é só salvar o w2i
+
+def load_vocabulary(filename: str) -> Vocabulary:
+   vocab = Vocabulary()
+   with open(filename) as f:
+      # Carregue o w2i e gere o i2w a partir dele
+      vocab.w2i = json.load(f)
+      vocab.i2w = {id: word for word, id in vocab.w2i.items()}
+
+   return vocab
+
+from typing import Iterable
+from cap19 import Layer, Tensor, random_tensor, zeros_like
+
+class Embedding(Layer):
+   def __init__(self, num_embeddings: int, embedding_dim: int) -> None:
+      self.num_embeddings = num_embeddings
+      self.embedding_dim = embedding_dim
+
+      # Um vetor de tamanho embedding_dim para cada incorporação desejada
+      self.embeddings = random_tensor(num_embeddings, embedding_dim)
+      self.grad = zeros_like(self.embeddings)
+
+      # Salve o ID da última entrada
+      self.last_input_id = None
+
+   def forward(self, input_id: int) -> Tensor:
+      """Basta selecionar o vetor de incorporação correspondente ao id da entrada"""
+      self.input_id = input_id               # lembre-se para usar na retropropagação
+
+      return self.embeddings[input_id]
+   
+   def backward(self, gradient: Tensor) -> None:
+      # Zere o gradiente correspondente à última entrada
+      # Isso é muito mais barato do que criar um tensor sempre que necessário
+      if self.last_input_id is not None:
+         zero_row = [0 for _ in range(self.embedding_dim)]
+         self.grad[self.last_input_id] = zero_row
+
+      self.last_input_id = self.input_id
+      self.grad[self.input_id] = gradient
+
+   def params(self) -> Iterable[Tensor]:
+      return [self.embeddings]
+   
+   def grads(self) -> Iterable[Tensor]:
+      return [self.grad]
+   
+class TextEmbedding(Embedding):
+   def __init__(self, vocab: Vocabulary, embedding_dim: int) -> None:
+      # Chame o construtor da superclasse
+      super().__init__(vocab.size, embedding_dim)
+
+      # E continue com ovaocabulário
+      self.vocab = vocab
+
+   def __getitem__(self, word: str) -> Tensor:
+      word_id = self.vocab.get_id(word)
+      if word_id is not None:
+         return self.embeddings[word_id]
+      else:
+         return None
+      
+   
